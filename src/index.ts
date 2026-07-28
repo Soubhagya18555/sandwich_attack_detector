f424aadab403cf4c8f765722dbf1f052ca9dd19a
Soubhagya <143580668+Soubhagya18555@users.noreply.github.com>
2026-07-28 11:23:00 +0530

    feat: expand modules and polish profile surface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from "./detector/pattern_matcher.js";
 export * from "./scoring/risk_scorer.js";
+export * from "./profiler/victim_profiler.js";
+export * from "./mempool/mempool_window.js";
+export * from "./alerts/alerts.js";
